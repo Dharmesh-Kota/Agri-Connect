f062ac6731f6c243cafd4b5355429250d072f640
Dharmesh <dharmeshkota123@gmail.com>
2024-08-10 07:57:02 +0530

    Template ready!

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import "aos/dist/aos.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../CSS/AboutUs.css";
+
+function Home() {
+    return (
+        <p> Hii!</p>
+    );
+}
+export default Home;
